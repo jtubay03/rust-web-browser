f389890bf99eed20932f3b1c87745152ddc809a5
jtubay <justin.03.tubay@gmail.com>
2023-10-16 01:32:47 -0400

    init web browser

---
+++ src/html_parse.rs
@@ -0,0 +1,9 @@
+use dom::{AttrMap, ElementData, Node, NodeType};
+
+use std::iter::Peekable;
+use std::str::Chars;
+
+pub struct HtmlParser<'a> {
+    chars: Peekable<Chars<'a>>,
+    node_q: Vec<String>,
+}
